cc506fa688ab639d3c0d7f8ca8011e9b388c0194
KB <kb@prashants-MacBook-Pro-2.local>
2024-04-09 14:29:39 +0530

    Question2

---
+++ src/Question2.js
@@ -0,0 +1,77 @@
+class Guest {
+  age;
+  constructor(age) {
+    this.age = age;
+  }
+  getAge() {
+    return this.age;
+  }
+}
+
+class Ticket {
+  guestCount;
+  guestAges;
+  totalPrice;
+
+  constructor(guests) {
+    this.guestCount = guests.length;
+    let guestAges = [],
+      totalPrice = 0;
+    for (let i = 0; i < guests.length; i++) {
+      guestAges.push(guests[i].getAge());
+      totalPrice += this.calculatePrice(guests[i].getAge());
+    }
+    this.guestAges = guestAges;
+    this.totalPrice = totalPrice;
+  }
+
+  calculatePrice(age) {
+    let price;
+    if (age < 0) {
+      throw new Error("Invalid age");
+    } else if (age >= 0 && age <= 2) {
+      price = 0;
+    } else if (age > 2 && age < 18) {
+      price = 100;
+    } else if (age >= 18 && age < 60) {
+      price = 500;
+    } else {
+      price = 300;
+    }
+    return price;
+  }
+  getTotalPrice() {
+    return this.totalPrice;
+  }
+  getGuestAges() {
+    return this.guestAges;
+  }
+  getGuestCount() {
+    return this.guestCount;
+  }
+  showTicket(){
+    for(let i=0;i<this.guestCount;i++){
+        console.log(` Guest1 :${i+1}( Age :${this.guestAges[i]})`);
+    }
+    console.log(` Total Price :${this.totalPrice}`);
+  }
+}
+
+class TicketCounter {
+  issueTicket(guests) {
+    const ticket = new Ticket(guests);
+    console.log("Ticket Issued");
+    console.log(`Total Guests :${ticket.getGuestCount()} Total Price :${ticket.getTotalPrice()}`);
+    return ticket;
+  }
+}
+class TicketChecker {
+  checkTicket(ticket) {
+    ticket.showTicket();
+  }
+}
+const guests = [new Guest(1),new Guest(3),new Guest(5),new Guest(25),new Guest(35),new Guest(65)];
+const ticketCounter = new TicketCounter();
+const ticket = ticketCounter.issueTicket(guests);
+const ticketChecker = new TicketChecker() ;
+ticketChecker.checkTicket(ticket);
